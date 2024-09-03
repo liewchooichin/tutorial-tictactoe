0a0de9db01a515e508a150c9da9af5e7755a2e0f
liewchooichin <liewchooichin@gmail.com>
2024-09-03 16:14:15 +0800

    add prop types for Copyright

--- src/QuotationApp/Copyright.jsx
+++ src/QuotationApp/Copyright.jsx
@@ -1,5 +1,9 @@
+import PropTypes from "prop-types"
 
-
+Copyright.propTypes = {
+    year: PropTypes.string,
+    author: PropTypes.string
+}
 export function Copyright({year="", author=""}){
     return(
         <div>
