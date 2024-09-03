9aa3f60258e9ddf0cc7b9e6069cc90ccbb03e21d
liewchooichin <liewchooichin@gmail.com>
2024-09-03 13:34:41 +0800

    edit person object in Gallery function

--- src/components/Gallery_v2.jsx
+++ src/components/Gallery_v2.jsx
@@ -29,25 +29,24 @@ export function Avatar({person, size=100}){
  */
 
 ProfileCard.propTypes = {
-  personName: PropTypes.object.isRequired,
-  imageId: PropTypes.string.isRequired,
+  person: PropTypes.object.isRequired,
   size: PropTypes.number,
   profession: PropTypes.string,
   awards: PropTypes.array,
   discovery: PropTypes.string
 }
+
 //Awards {awards.length}: {awards.join(", ")} <br/>
-export function ProfileCard({imageId, personName, size=70,
-  profession, awards, discovery}){
+export function ProfileCard({person, size=70,
+  profession="", awards="", discovery=""}){
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={getImageUrl(imageId)} />
+      <Card.Img variant="top" src={getImageUrl(person)} />
       <Card.Body>
-        <Card.Title>{personName}</Card.Title>
+        <Card.Title>{person.name}</Card.Title>
         <Card.Text>
           Profession: {profession}<br/>
-          Awards {awards}<br/>
-          <i>Problems using array in components</i><br/>
+          Awards {awards}
           Discover: {discovery}
         </Card.Text>
         <Button variant="primary">More info</Button>
@@ -58,15 +57,19 @@ export function ProfileCard({imageId, personName, size=70,
 
 export function Gallery() {
   const galleryName = "A Gallery";
-
+  
   return (
       <section>
       <h1>{galleryName}</h1>
-      <ProfileCard name='Lin Lanying' imageId='1bX5QH6' size={100} />
-      <ProfileCard name='Aklilu Lemma' imageId='OKS67lh' size={80} />
-      <ProfileCard name='Katsuko Saruhashi' imageId='YfeOqp2' size={50} />
+      <ProfileCard person={{"name":'Lin Lanying', imageId:'1bX5QH6'}}/>
+      <ProfileCard person={{name:'Aklilu Lemma', imageId:'OKS67lh'}} />
+      <ProfileCard person={{name:'Katsuko Saruhashi', imageId:'YfeOqp2'}} 
+      />
       <ProfileCard
-        imageId="szV5sdG" personName="Maria Skłodowska-Curie"
+        person={{
+          imageId: "szV5sdG", 
+          personName: "Maria Skłodowska-Curie"
+        }}
         profession="physicist and chemist"
         discovery="polonium (chemical element)"
         awards={[
@@ -77,8 +80,10 @@ export function Gallery() {
         ]}
       />
       <ProfileCard
-        imageId='YfeOqp2'
-        personName='Katsuko Saruhashi'
+        person={{
+          imageId:'YfeOqp2',
+          personName:'Katsuko Saruhashi'
+        }}
         profession='geochemist'
         discovery="a method for measuring carbon dioxide in seawater"
         awards={[
