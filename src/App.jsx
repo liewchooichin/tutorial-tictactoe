de70d7cdb85dcf084b81313107380b040502af89
liewchooichin <liewchooichin@gmail.com>
2024-09-08 22:16:14 +0800

    form fields and button control with status.

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,21 @@ import Container from 'react-bootstrap/Container'
 import { ListAllStatus } from "./components/Chap3Statuses"
 import { FormWater } from './components/Chap3FormWater'
 import { BackgroundPicture } from './components/Chap3BackgroundPicture'
-
+import { FieldEditor } from './components/Chap3FieldEditor'
 
 function App() {
 
   return (
     <Container>
     <h1 style={{marginBottom:"1em"}}>Chapter 3: Managing States</h1>
+
+    <h2>Field name editor</h2>
+    <p>The field is enabled or disabled, the text in the button 
+      changed according to the state.
+    </p>
+    <div style={{marginBottom:"2em"}}>
+      <FieldEditor></FieldEditor>
+    </div>
 
     <h2>Activating background or border of picture</h2>
     <p>Clicking on the background or the picture will change the appearance of the element.
