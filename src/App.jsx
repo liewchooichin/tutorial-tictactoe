2acc63d3a4fd80de64abd7174da93ddfbd6e6938
liewchooichin <liewchooichin@gmail.com>
2024-09-09 22:40:36 +0800

    Star and unstar a row. Highlight a row using onFocus and onPointerMove.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,23 @@ import Container from 'react-bootstrap/Container'
 /* Tutorial: Chapter 3-2 */
 import { TimeColor } from './components/Chap32ChangeTimeColor';
 import { PackingList } from './components/Chap32PackingList';
-
+import { StarLetter } from './components/Chap32StarLetter';
 
 function App() {
 
   return (
     <Container>
     <h1 style={{marginBottom:"1em"}}>Chapter 3: Managing States</h1>
+
+    <h2>Star and unstar a letter</h2>
+    <p>The row should be correctly highlighted when clicking on
+      star or unstar.<br/>
+      The row will be highlighted according to Focus(tab) or Pointer
+      Move.
+    </p>
+    <div style={{marginBottom:"2em"}}>
+      <StarLetter></StarLetter>
+    </div>
 
     <h2>Packing list</h2>
     <p>The summary must be correct according to the item checked 
---
+++ src/components/dataLetter.jsx
@@ -0,0 +1,22 @@
+export const initialLetters = [{
+    id: 0,
+    subject: 'Ready for adventure?',
+    isStarred: true,
+  }, {
+    id: 1,
+    subject: 'Time to check in!',
+    isStarred: false,
+  }, {
+    id: 2,
+    subject: 'Festival Begins in Just SEVEN Days.',
+    isStarred: false,
+  },{
+    id: 3,
+    subject: 'Workshop on making your own terrarium.',
+    isStarred: false,
+  },{
+    id: 4,
+    subject: 'Book club promotion ends in 3 days.',
+    isStarred: false,
+  }];
+  
