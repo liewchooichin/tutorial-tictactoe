eeaa0e8315085c915623e6667ad9d52f8b93a36b
liewchooichin <liewchooichin@gmail.com>
2024-09-09 11:40:53 +0800

    Put form components and handlers in a separate function.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import Container from 'react-bootstrap/Container'
 
 /* Tutorial: Chapter 3-2 */
 import { FeedbackForm } from './components/Chap32SendingStatus';
+import { SnackMenu } from './components/Chapter32DoNotDuplicate';
+
 
 function App() {
 
   return (
     <Container>
     <h1 style={{marginBottom:"1em"}}>Chapter 3: Managing States</h1>
+
+    <h2>Do not duplicate contents in states</h2>
+    <p>
+    </p>
+    <div style={{marginBottom:"2em"}}>
+      <SnackMenu></SnackMenu>
+    </div>
 
     <h2>The sending and isSending status will update the form.</h2>
     <p>Somehow, when the the button type="submit", the handler will
@@ -20,8 +29,6 @@ function App() {
     <div style={{marginBottom:"2em"}}>
       <FeedbackForm></FeedbackForm>
     </div>
-
-
 
     </Container>
   );
