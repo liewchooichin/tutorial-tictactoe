9e381937e621e7ee8090d3759632cf6e39c99984
liewchooichin <liewchooichin@gmail.com>
2024-09-11 22:56:35 +0800

    Using Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,54 +3,18 @@ import viteLogo from '/vite.svg'
 //import './App.css'
 import Container from 'react-bootstrap/Container'
 
-/* Tutorial: Chapter 3-4 */
-import { DisappearingInputText } from 
-"./components/Chap34DisappearingInputText";
-import { SwapTwoFormFields } from
-"./components/Chap34SwapTwoFormFields";
-import { ContactManager } from 
-  './components/Chap34ResetDetailForm';
-import { PlacesGallery } from 
-  './components/Chap34ClearImageWhileLoading';
-import { EmailContactList } from 
-  './components/Chap34MisplacedStateInList';
+/* Tutorial: Chapter 3-5 */
+import { TaskListApp } from './components/Chap35TaskListWithReducer';
 
 
 function App() {
 
   return (
     <Container>
-    <h2>Fix misplaced state in the list</h2>
-    <p> 
-    </p>
-    <div style={{marginBottom:"2em"}}>
-      <EmailContactList></EmailContactList>
-    </div>
 
-    <h2>Clear an image while it’s loading</h2>
-    <p> 
-    </p>
+    <h2>Task events using useReducer</h2>
     <div style={{marginBottom:"2em"}}>
-      <PlacesGallery></PlacesGallery>
-    </div>
-
-    <h2>Reset a detail form</h2>
-    <p>The Reset button will not &quot;reset&quot; if the data has
-      been &quot;saved&quot;. 
-    </p>
-    <div style={{marginBottom:"2em"}}>
-      <ContactManager></ContactManager>
-    </div>
-
-    <h2>Swap two form fields</h2>
-    <div style={{marginBottom:"2em"}}>
-      <SwapTwoFormFields></SwapTwoFormFields>
-    </div>
-
-
-    <h2>Fix disappearing input text</h2>
-    <div style={{marginBottom:"2em"}}>
-      <DisappearingInputText></DisappearingInputText>
+      <TaskListApp></TaskListApp>
     </div>
 
 
---
+++ src/components/Chap35TaskList.jsx
@@ -0,0 +1,182 @@
+import PropTypes from "prop-types";
+import { useState, useReducer } from 'react';
+//import AddTask from './AddTask.js';
+//import TaskList from './TaskList.js';
+
+AddTask.propTypes = {
+    onAddTask: PropTypes.func.isRequired
+}
+function AddTask({onAddTask}) {
+    const [text, setText] = useState('');
+    return (
+      <>
+        <input
+          placeholder="Add task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setText('');
+            onAddTask(text);
+          }}>
+          Add
+        </button>
+      </>
+    );
+  }
+  
+
+  TaskList.propTypes = {
+    tasks: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            done: PropTypes.bool.isRequired
+        }),
+    ),
+    onChangeTask: PropTypes.func.isRequired,
+    onDeleteTask: PropTypes.func.isRequired
+  }
+  function TaskList({tasks, onChangeTask, onDeleteTask}) {
+    return (
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <Task task={task} onChange={onChangeTask} onDelete={onDeleteTask} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  
+  Task.propTypes = {
+    task: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            done: PropTypes.bool.isRequired
+        }),
+    onChange: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired
+  }
+  function Task({task, onChange, onDelete}) {
+    const [isEditing, setIsEditing] = useState(false);
+    let taskContent;
+    if (isEditing) {
+      taskContent = (
+        <>
+          <input
+            value={task.text}
+            onChange={(e) => {
+              onChange({
+                ...task,
+                text: e.target.value,
+              });
+            }}
+          />
+          <button onClick={() => setIsEditing(false)}>Save</button>
+        </>
+      );
+    } else {
+      taskContent = (
+        <>
+          {task.text}
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+        </>
+      );
+    }
+    return (
+      <label>
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={(e) => {
+            onChange({
+              ...task,
+              done: e.target.checked,
+            });
+          }}
+        />
+        {taskContent}
+        <button onClick={() => onDelete(task.id)}>Delete</button>
+      </label>
+    );
+  }
+
+
+
+
+export function TaskListApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: 'changed',
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: 'deleted',
+      id: taskId,
+    });
+  }
+
+  return (
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
+
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case 'added': {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case 'changed': {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case 'deleted': {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
+
+let nextId = 3;
+const initialTasks = [
+  {id: 0, text: 'Visit Kafka Museum', done: true},
+  {id: 1, text: 'Watch a puppet show', done: false},
+  {id: 2, text: 'Lennon Wall pic', done: false},
+];
