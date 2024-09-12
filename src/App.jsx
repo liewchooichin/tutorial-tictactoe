4eec2f5102a2752097f739a3e2cd77ce46e75836
liewchooichin <liewchooichin@gmail.com>
2024-09-12 18:31:05 +0800

    Place images using context.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,20 @@ import Container from 'react-bootstrap/Container'
 /* Tutorial: Chapter 3-5 */
 import { PageApp } from './components/Chap36ContextHeading/PageApp';
 import { BlogPostApp } from './components/Chap36ContextBlogPost/BlogPostApp';
+import { PlaceImageApp } from './components/Chap36ContextImages/PlaceImageApp';
+
 
 function App() {
 
   return (
     <Container>
+
+    <h2>Place Image App</h2>
+    <p>Using context
+    </p>
+    <div style={{marginBottom:"2em"}}>
+      <PlaceImageApp></PlaceImageApp>
+    </div>
 
     <h2>Blog Post App</h2>
     <p>Using context
